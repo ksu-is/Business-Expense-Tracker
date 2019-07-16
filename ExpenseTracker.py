611a9b53b9022e4f7a727189e619773ce31a2eca
dmccumbe <51388690+dmccumbe@users.noreply.github.com>
2019-07-16 18:14:42 -0400

    Create ExpenseTracker.py

---
+++ ExpenseTracker.py
@@ -0,0 +1,52 @@
+# Used original code from Ivaylo Spasov and modified into a working program
+
+
+# enter the monthly budget provided
+currentBudget = 40000
+
+
+
+def main():
+    print('Welcome to Expense Tracker 1.0! ')
+
+    closeProgram = 'no'
+    totalBudget = currentBudget
+
+    while closeProgram == 'no': 
+        print()
+        print('Expense Tracker Selections: ')
+        print('1- Add your Monthly Expenses Total: ')
+        print('2- Check your remaining Monthly Budget: ')
+        print('3- Exit Expense Tracker: ')
+        print()
+
+        choice = int(input('Proceed by entering a selection:'))
+        if choice == 1:
+            print()
+            totalBudget = addExpense(totalBudget)
+
+        elif choice == 2:
+            print()
+            print('Your current monthly budget is {40000}'.format(totalBudget))
+        
+        elif choice == 3:
+            print()
+            closeProgram = 'yes'
+            print('Expense tracker will now close. Have a nice day!')
+
+        else:
+            print('You have not entered a valid selection, please do so again')
+
+def addExpense(totalBudget):
+    expense = float(input('Enter your total monthly expenses amount: $'))
+    totalExpense = expense
+    if totalBudget - totalExpense >= 0:
+        totalBudget = totalBudget - totalExpense
+        print('Your business expenses have been recorded, your remaining total for the month is: ${0}'.format(totalBudget))
+        return totalBudget
+
+    else:
+        print('You have exceeded your monthly budget!')
+        return totalBudget
+
+main()
